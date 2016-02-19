d03b07a23e9c9f5f5f187ff56563fc48df4312cb
Oli Donald <widosm@widosm.com>
2016-02-19 20:52:00 +0000

    Added StringState enum and some constants http://ericlippert.com/2016/02/19/cellar/#more-3822

--- ZMachine/ZString.swift
+++ ZMachine/ZString.swift
@@ -6,7 +6,24 @@
 typealias WordZStringAddress = Int
 typealias ZStringAddress = Int
 
-let alphabet_table = [" ", "^", "^", "*", "*", "?", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let alphabet_table = [
+    [" ", "?", "?", "?", "?", "?", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    [" ", "?", "?", "?", "?", "?", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    [" ", "?", "?", "?", "?", "?", "\n", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "!", "?", "_", "#", "'", "\"", "/", "\\", "-", ":", "(", ")"]]
+
+enum StringState {
+    case Alphabet(Int)
+    case Abbrev(AbbreviationNumber)
+    case Leading
+    case Trailing(Int)
+}
+
+let abbrev0 = StringState.Abbrev(AbbreviationNumber(0))
+let abbrev32 = StringState.Abbrev(AbbreviationNumber(32))
+let abbrev64 = StringState.Abbrev(AbbreviationNumber(64))
+let alphabet0 = StringState.Alphabet(0)
+let alphabet1 = StringState.Alphabet(1)
+let alphabet2 = StringState.Alphabet(2)
 
 struct ZString {
     static func abbreviations_table_base(story: Story) -> AbbreviationTableBase {
@@ -32,7 +49,7 @@ struct ZString {
             let zchar1 = fetch_bits(bit14, length: size5, word: word)
             let zchar2 = fetch_bits(bit9, length: size5, word: word)
             let zchar3 = fetch_bits(bit4, length: size5, word: word)
-            let s = acc + alphabet_table[zchar1] + alphabet_table[zchar2] + alphabet_table[zchar3]
+            let s = acc + alphabet_table[0][zchar1] + alphabet_table[0][zchar2] + alphabet_table[0][zchar3]
             if is_end == 1 {
                 return s
             }
